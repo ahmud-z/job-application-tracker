60f1230bcfafcb11262a68a576b02bb37810189a
Ahmudul Hossain <ahmudulhossain.n5@gmail.com>
2026-01-26 20:41:42 +0600

    intgrate add job application feature in frontend

--- frontend/src/components/ApplicationForm.jsx
+++ frontend/src/components/ApplicationForm.jsx
@@ -1,11 +1,28 @@
-import React from "react";
-import { Link } from "react-router";
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router";
 
 const ApplicationForm = () => {
 
+    const navigate = useNavigate();
+    const [applicationInfo, setApplicationInfo] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(applicationInfo);
+
+        axios.post("http://localhost:3000/api/create", applicationInfo).then((res) => {
+            console.log(res);
+            navigate("/")
+
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
+
     return (
         <div className="flex justify-center p-6">
-            <form className="w-full max-w-md bg-white rounded-xl shadow-lg p-6 space-y-4">
+            <form onSubmit={handleSubmit} className="w-full max-w-md bg-white rounded-xl shadow-lg p-6 space-y-4">
                 <h2 className="text-xl font-semibold text-center">
                     Add Job Application
                 </h2>
@@ -13,6 +30,8 @@ const ApplicationForm = () => {
                 <div className="space-y-1">
                     <label className="text-sm font-medium">Company Name</label>
                     <input
+                        value={applicationInfo.company_name}
+                        onChange={(e) => setApplicationInfo({ ...applicationInfo, company_name: e.target.value })}
                         required
                         type="text"
                         placeholder="Google"
@@ -23,6 +42,8 @@ const ApplicationForm = () => {
                 <div className="space-y-1">
                     <label className="text-sm font-medium">Job Role</label>
                     <input
+                        value={applicationInfo.position}
+                        onChange={(e) => setApplicationInfo({ ...applicationInfo, position: e.target.value })}
                         required
                         type="text"
                         placeholder="Frontend Developer"
@@ -32,12 +53,21 @@ const ApplicationForm = () => {
 
                 <div className="space-y-1">
                     <label className="text-sm font-medium">Status</label>
-                    <select className="w-full rounded-md border border-gray-300 px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500">
-                        <option value="">Select status</option>
-                        <option>Applied</option>
-                        <option>Interview</option>
-                        <option>Offer</option>
-                        <option>Rejected</option>
+                    <select
+                        value={applicationInfo.status || ""}
+                        onChange={(e) =>
+                            setApplicationInfo({
+                                ...applicationInfo,
+                                status: e.target.value
+                            })
+                        }
+                        className="w-full rounded-md border border-gray-300 px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                    >
+                        <option value="" disabled>Select status</option>
+                        <option value="Applied">Applied</option>
+                        <option value="Interview">Interview</option>
+                        <option value="Offer">Offer</option>
+                        <option value="Rejected">Rejected</option>
                     </select>
                 </div>
 
@@ -46,6 +76,8 @@ const ApplicationForm = () => {
                         Application Date
                     </label>
                     <input
+                        value={applicationInfo.applied_date}
+                        onChange={(e) => setApplicationInfo({ ...applicationInfo, applied_date: e.target.value })}
                         required
                         type="date"
                         className="w-full rounded-md border border-gray-300 px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
@@ -55,6 +87,8 @@ const ApplicationForm = () => {
                 <div className="space-y-1">
                     <label className="text-sm font-medium">Salary Range</label>
                     <input
+                        value={applicationInfo.salary_range}
+                        onChange={(e) => setApplicationInfo({ ...applicationInfo, salary_range: e.target.value })}
                         required
                         type="text"
                         placeholder="$60k - $80k"
@@ -65,6 +99,8 @@ const ApplicationForm = () => {
                 <div className="space-y-1">
                     <label className="text-sm font-medium">Location</label>
                     <input
+                        value={applicationInfo.location}
+                        onChange={(e) => setApplicationInfo({ ...applicationInfo, location: e.target.value })}
                         required
                         type="text"
                         placeholder="Remote / New York"
@@ -75,6 +111,8 @@ const ApplicationForm = () => {
                 <div className="space-y-1">
                     <label className="text-sm font-medium">Notes</label>
                     <textarea
+                        value={applicationInfo.notes}
+                        onChange={(e) => setApplicationInfo({ ...applicationInfo, notes: e.target.value })}
                         rows="3"
                         placeholder="Any extra details..."
                         className="w-full rounded-md border border-gray-300 px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
