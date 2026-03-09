c531c3735c01c184b76a39193a2203b405ab7448
Ahmudul Hossain <ahmudulhossain.n5@gmail.com>
2026-03-10 01:35:03 +0600

    fix: loopback route navigation fixed

--- frontend/src/components/DashboardPage.jsx
+++ frontend/src/components/DashboardPage.jsx
@@ -31,7 +31,7 @@ const DashboardPage = () => {
                 setCountByStatus(counts);
             })
             .catch(err => {
-                console.log(err.message);
+                localStorage.removeItem('token')
                 navigate("/login");
             });
     }, []);
